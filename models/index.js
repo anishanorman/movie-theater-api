2ff9299e2f594887e4644ea129ead1eef21d4fd1
Anisha Norman <anisha.norman@hl.co.uk>
2022-11-11 11:28:57 +0000

    Updated User-show relationship to be many-many

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 const { Show } = require('./Show')
 const { User } = require('./User')
 
-Show.belongsTo(User)
-User.hasMany(Show)
+Show.belongsToMany(User, { through: "userShows" })
+User.belongsToMany(Show, { through: "userShows" })
 
 module.exports = {Show, User}
