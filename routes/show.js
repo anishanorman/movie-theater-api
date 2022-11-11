ddaa99945f087529b71914a8c2b0898c73f3bc31
Anisha Norman <anisha.norman@hl.co.uk>
2022-11-11 14:14:02 +0000

    findAll shows

--- routes/show.js
+++ routes/show.js
@@ -1,4 +1,9 @@
 const { Router } = require("express")
 const showRouter = Router()
+const { Show } = require("../models/Show")
+
+showRouter.get("/", async (req, res) => {
+    res.send(await Show.findAll());
+  });
 
 module.exports = showRouter
