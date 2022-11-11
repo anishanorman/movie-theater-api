f85acaaf5efc8c02cca7cae3c8321a21fb93cf9c
Anisha Norman <anisha.norman@hl.co.uk>
2022-11-11 14:15:43 +0000

    Find a specific show

--- routes/show.js
+++ routes/show.js
@@ -4,6 +4,12 @@ const { Show } = require("../models/Show")
 
 showRouter.get("/", async (req, res) => {
     res.send(await Show.findAll());
-  });
+});
+
+showRouter.get("/:showId", async (req, res) => {
+    const showId = req.params.showId
+    const show = await Show.findByPk(showId)
+    res.send(show)
+})
 
 module.exports = showRouter
